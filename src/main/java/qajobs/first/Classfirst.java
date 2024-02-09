b2c12a4fb86dc9b8ecfb56ea5fef45596e0c6269
at <at2kool@hotmail.com>
2024-02-09 01:07:50 -0800

    test commit

---
+++ src/main/java/qajobs/first/Classfirst.java
@@ -0,0 +1,11 @@
+package qajobs.first;
+
+public class Classfirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("QA jobs in Lahore");
+
+	}
+
+}
