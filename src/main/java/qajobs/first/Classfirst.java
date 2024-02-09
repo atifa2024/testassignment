ea1f1b9d99bf7dc1c03f68b99fc845dac090509c
at <at2kool@hotmail.com>
2024-02-09 03:19:03 -0800

    changes

--- src/main/java/qajobs/first/Classfirst.java
+++ src/main/java/qajobs/first/Classfirst.java
@@ -5,6 +5,7 @@ public class Classfirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("QA jobs in Lahore");
+	System.out.print(("hello"));
 
 	}
 
